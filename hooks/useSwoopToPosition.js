e4059848d4ecde5b1d1029b96c4482e0d8e665fa
George Campbell <campbell.goe@gmail.com>
2020-03-06 13:13:14 +0000

    formatting

--- hooks/useSwoopToPosition.js
+++ hooks/useSwoopToPosition.js
@@ -3,31 +3,26 @@ import { useState, useEffect } from "react";
 import getDist from "../utils/getDist.js";
 //swoop to position with ease
 function useSwoopToPosition(
-  position          ,
-  setPosition          ,
+  position,
+  setPosition,
   {
     swoopOnStart = true,
     easeAmount = 4,
     onTargetReached,
     targetRadius = 1
-  }   
-                           
-                        
-                               
-                         
-   ,
-  deps    
+  },
+  deps
 ) {
-  const [swoopToPosition, setSwoopToPosition]                      = useState(
+  const [swoopToPosition, setSwoopToPosition] = useState(
     swoopOnStart
   );
   useEffect(() => {
     if (swoopToPosition) {
-      const targetPos            = position();
+      const targetPos = position();
       setPosition(({ x: currentX, y: currentY }) => {
         const outputPos = {
           x: currentX + (targetPos.x - currentX) / easeAmount,
-          y: currentY + (targetPos.y - currentY) / easeAmount
+          y: currentY + (targetPos.y - currentY) / easeAmount,
         };
         if (typeof onTargetReached == "function") {
           const dist = getDist(outputPos, targetPos);
